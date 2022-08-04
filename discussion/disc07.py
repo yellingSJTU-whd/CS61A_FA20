fbec3dde5e7520a24b9191bb8e5b1d188ee1759d
10300749 <wang.haode@zte.com.cn>
2022-08-04 11:14:16 +0800

    CS61A disc07

---
+++ discussion/disc07.py
@@ -0,0 +1,158 @@
+class MinList:
+    """A  list that can only pop the smallest element"""
+
+    def __init__(self):
+        self.items = []
+        self.size = 0
+
+    def append(self, item):
+        """Appends an item to the MinList
+        >>> m = MinList()
+        >>> m.append(4)
+        >>> m.append(2)
+        >>> m.size
+        2
+        """
+        self.items.append(item)
+        self.items = sorted(self.items)
+        self.size += 1
+
+    def pop(self):
+        """ Removes and returns the smallest item from the MinList
+        >>> m = MinList()
+        >>> m.append(4)
+        >>> m.append(1)
+        >>> m.append(5)
+        >>> m.pop()
+        1
+        >>> m.size
+        2
+        """
+        if self.size:
+            self.size -= 1
+            return self.items.pop(0)
+
+
+class Email:
+    """Every email object has 3 instance attributes: the
+    message, the sender name, and the recipient name.
+    """
+
+    def __init__(self, msg, sender_name, recipient_name):
+        self.msg = msg
+        self.sender_name = sender_name
+        self.recipient_name = recipient_name
+
+
+class Server:
+    """Each Server has an instance attribute clients, which
+    is a dictionary that associates client names with
+    client object.
+    """
+
+    def __init__(self):
+        self.clients = {}
+
+    def send(self, email):
+        """Take an email and put it in the inbox of the client
+        it is addressed to.
+        """
+        client = self.clients[email.recipient_name]
+        client.receive(email)
+
+    def register_client(self, client, client_name):
+        if client and client_name:
+            self.clients[client_name] = client
+
+
+class Client:
+    """Every Client has instance attributes name (which is
+    used for addressing emails to the client), server
+    (which is used to send emails out to other clients), and
+    inbox (a list of all emails the client has received).
+    """
+
+    def __init__(self, server, name):
+        self.inbox = []
+        self.server = server
+        self.name = name
+
+    def compose(self, msg, recipient_name):
+        """Send an email with the given message msg to the
+        given recipient client.
+        """
+        email = Email(msg, self.name, recipient_name)
+        self.server.send(email)
+
+    def receive(self, email):
+        """Take an email and add it to the inbox of this
+        client.
+        """
+        if email:
+            self.inbox.append(email)
+
+
+class Pet():
+    def __init__(self, name, owner):
+        self.is_alive = True  # It's alive!!!
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name)
+
+
+class Dog(Pet):
+    def talk(self):
+        print(self.name + ' says woof!')
+
+
+class Cat(Pet):
+    def __init__(self, name, owner, lives=9):
+        super().__init__(name, owner)
+        self.lives = lives
+
+    def talk(self):
+        print(self.name + ' says meow!')
+
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero, 'is_alive'
+        becomes False. If this is called after lives has reached zero, print out
+        that the cat has no more lives to lose.
+        """
+        if self.lives:
+            self.lives -= 1
+            if not self.lives:
+                self.is_alive = False
+        else:
+            print(self.name + ' has no more lives to lose')
+
+
+class NoisyCat(Cat):
+    """A Cat that repeats things twice."""
+
+    def talk(self):
+        """Talks twice as much as a regular cat.
+
+        >>> NoisyCat('Magic', 'James').talk()
+        Magic says meow!
+        Magic says meow!
+        """
+        for _ in range(2):
+            super().talk()
+
+    def __repr__(self):
+        """
+        The interpreter-readable representation of a NoisyCat
+
+        >>> muffin = NoisyCat('Muffin', 'Catherine')
+        >>> repr(muffin)
+        "NoisyCat('Muffin', 'Catherine')"
+        >>> muffin
+        NoisyCat('Muffin', 'Catherine')
+        """
+
+        return "NoisyCat('{}', '{}')".format(self.name, self.owner)
