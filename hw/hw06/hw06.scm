6a989f339ef1412ba14bd6d9ab0f9c53ac252a36
10300749 <wang.haode@zte.com.cn>
2022-08-22 09:06:55 +0800

    CS61A HW06

--- hw/hw06/hw06.scm
+++ hw/hw06/hw06.scm
@@ -1,23 +1,31 @@
 (define (cddr s)
-  (cdr (cdr s)))
+    (cdr (cdr s)))
 
 (define (cadr s)
-  'YOUR-CODE-HERE
-)
+    (car (cdr s))
+    )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
-)
+    (car (cdr (cdr s)))
+    )
 
 
 (define (sign num)
-  'YOUR-CODE-HERE
-)
+    (cond
+        ((< num 0) -1)
+        ((> num 0) 1)
+        (else 0))
+    )
 
 
 (define (square x) (* x x))
 
 (define (pow x y)
-  'YOUR-CODE-HERE
-)
+    (cond
+        ((= y 1) x)
+        ((= y 2) (square x))
+        ((even? y) (square (pow x (/ y 2))))
+        ((odd? y) (* x (square (pow x (/ (- y 1) 2)))))
+        )
+    )
 
