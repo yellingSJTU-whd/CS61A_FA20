a919e1ac91cf71b64efd258c9073fde57a8448bb
10300749 <wang.haode@zte.com.cn>
2022-08-23 10:21:13 +0800

    CS61A disc10

--- discussion/disc10.scm
+++ discussion/disc10.scm
@@ -9,3 +9,28 @@
         ((= n 0) 0)
         ((= n 1) 1)
         (else (+ (fib (- n 1)) (fib (- n 2))))))
+
+(define (my-append a b)
+    (define (helper a b)
+        (if (eq? (cdr a) nil)
+            (cons (car a) b)
+            (cons (car a) (helper (cdr a) b))))
+
+    (cond
+        ((eq? a nil) b)
+        ((eq? b nil) a)
+        (else (helper a b))
+        ))
+
+(define (duplicate lst)
+    (cond
+        ((eq? lst nil) nil)
+        ((eq? (cdr lst) nil) (cons (car lst) (cons (car lst) nil)))
+        (else (cons (car lst) (cons (car lst) (duplicate (cdr lst)))))
+        ))
+
+(define (insert element lst index)
+    (cond
+        ((= index 0) (cons element lst))
+        ((eq? lst nil) nil)
+        (else (insert element (cdr lst) (- index 1)))))
